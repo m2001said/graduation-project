a8f1ad63fb84a7531b8dc2cd72c541e8a5323645
m2001said <ms277712@gmail.com>
2023-11-27 17:09:09 +0200

    modifing h1 in app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>graduation project</h1>
+      <h1>graduation project | from said branch</h1>
     </div>
   );
 }
