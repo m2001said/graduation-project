dc27d49aee7eaca090144c3c684af45b4f43a890
m2001said <ms277712@gmail.com>
2023-12-01 13:36:18 +0200

    adding mainpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router";
 import Dashboard from "./pages/Dashboard";
+import MainPage from "./pages/MainPage";
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
