f6833f33336c60e154ea0d933f39de7017ee4835
Abdo-Hosni123 <abdohosni12384@gmail.com>
2023-12-02 23:20:58 +0200

    removing all iocns that related to mui

--- .history/src/components/sections/navbars/navbar1/Navbar1_20231202230636.jsx
+++ .history/src/components/sections/navbars/navbar1/Navbar1_20231202230636.jsx
@@ -5,10 +5,17 @@ import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+import CommentRoundedIcon from '@mui/icons-material/CommentRounded';
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 import Logo from '../../../../images/Logo.jpg';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import './navbar1.css'
+
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -17,26 +24,32 @@ const Navbar = () => {
   const menuOptions = [
     {
       text: "Home",
+      icon: <HomeIcon />,
     },
     {
       text: "Pages",
+      icon: <InfoIcon />,
     },
     {
       text: "Services",
+      icon: <CommentRoundedIcon />,
     },
     {
       text: "Project",
+      icon: <PhoneRoundedIcon />,
     },
     {
       text: "Blog",
+      icon: <ShoppingCartRoundedIcon />,
     },
     {
       text: "Contact",
+      icon: <ShoppingCartRoundedIcon />,
     },
   ];
 
   return (
-    <div>
+    <div  >
       <header>
         <div className="container">
           <a href="#" className="logo">
@@ -48,10 +61,14 @@ const Navbar = () => {
               <i className="fas fa-bars toggle-menu" onClick={() => setOpenMenu(!openMenu)}></i>
             )}
             <ul>
-              {menuOptions.map((item, index) => (
-                <li key={index}><a className={index === 0 ? "active" : ""} href="#">{item.text}</a></li>
-              ))}
+              <li><a className="active" href="#">Home</a></li>
+              <li><a href="#">Pages</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Project</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">Contact</a></li>
             </ul>
+           
           </nav>
         </div>
       </header>
@@ -68,9 +85,10 @@ const Navbar = () => {
           onKeyDown={() => setOpenMenu(false)}
         >
           <List>
-            {menuOptions.map((item, index) => (
-              <ListItem key={index} disablePadding>
+            {menuOptions.map((item) => (
+              <ListItem key={item.text} disablePadding>
                 <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
                   <ListItemText primary={item.text} />
                 </ListItemButton>
               </ListItem>
