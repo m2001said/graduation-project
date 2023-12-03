b2dffdb59a4483a5ef664956ba8937ca7850a08a
abdelrahmanrgab <abdelrahmanragab787@gmail.com>
2023-12-03 19:37:40 +0200

    add mycomponents to TrialDesign

--- src/pages/TrialDesign.jsx
+++ src/pages/TrialDesign.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Project1 from '../components/sections/projects/projects1/Project1' 
+import Articles from '../components/sections/Items/items1/Items1' 
+import Statictics1 from '../components/sections/statistics/statistics1/Statictics1' 
+
+
 
 const TrialDesign = () => {
   return (
-    <div>
-      <h1>صفحة التصميم الاولي ، ضيفوا ملفاتكم بترتيب التصميم لو سمحتم</h1>
-      <p>رجب وحسني والسيد سليمان</p>
-    </div>
+    <>
+<Project1 />
+<Statictics1 />
+<Articles /> 
+   </>
   );
 };
 
