deb565407db6b53d08e21b13966d272b81066b84
Your Name <you@example.com>
2023-12-06 17:35:11 +0200

    schema

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/mainPage/header/Header.jsx";
-import Section from "../components/mainPage/heroSection/MainSection.js";
+import Header from "../components/dashboard/header/Header.jsx";
+import Section from "../components/dashboard/heroSection/MainSection.js";
 const MainPage = () => {
   return (
     <>
