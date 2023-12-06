853c0a62b130d2ac011e57952a21fbd28df1040a
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2023-12-06 15:41:36 +0200

    delete cards folder

--- src/components/sections/pricings/pricing1/Pricing1.jsx
+++ src/components/sections/pricings/pricing1/Pricing1.jsx
@@ -1,6 +1,40 @@
 import React from 'react'
-import Cards from './cards/pricing-cards.jsx'
+import Vector1 from "../../../../assets/images/Vector.svg"
+import Vector2 from "../../../../assets/images/Vector2.svg"
 import "./pricing1.css"
+
+
+const cardData = [
+  {
+    title: "Design advices",
+    doller:"$",
+    price: "29",
+    features: ["General living space advices", "Renovation advices", "Interior design advices", "Furniture reorganization", "Up to 5 hours meetings"],
+    buttonClass: "button1",
+    Vector:Vector1,
+    cardClass: "pricingCard1",
+  },
+  {
+    title: "Complete interior",
+    doller:"$",
+    price: "39",
+    Plans: "Most Popular Plans",
+    features: ["Complete home redesign", "Interior and exterior works", "Modular interior planning", "Kitchen design", "Garages organization"],
+    buttonClass: "button2",
+    Vector:Vector2,
+    cardClass: "pricingCard2",
+  },
+  {
+    title: "Furniture design",
+    price: "59",
+    doller:"$",
+    features: ["Furniture for living room", "Furniture refurbishment", "Sofas and armchairs", "Tables and chairs", "Kitchens"],
+    buttonClass: "button3",
+    Vector:Vector1,
+    cardClass: "pricingCard3",
+  },
+];
+
 
 const pricing1 = () => {
   return (
@@ -9,7 +43,24 @@ const pricing1 = () => {
     <h1 >Pricing & Plan</h1>
     <p>Home / Priceing</p>
     </div>
-    <Cards />
+    <div className="pricingCard-container">
+      {cardData.map((card, index) => (
+        <div key={index} className={`pricingCard ${card.cardClass}`}>
+          <h3>{card.title}</h3>
+          <h1><span>{card.doller}</span>{card.price}</h1>
+          <span>/month</span>
+          <hr></hr>
+          <h6>{card.Plans}</h6>
+          {card.features.map((feature, i) => (
+            <p key={i}>{feature}</p>
+          ))}
+          <button className={card.buttonClass}>
+          <span> Get started </span>
+          <img src={card.Vector} alt="" /> 
+          </button>
+        </div>
+      ))}
+    </div>
    </div> 
   )
 }
--- src/components/sections/teams/teams1/Teams1.jsx
+++ src/components/sections/teams/teams1/Teams1.jsx
@@ -1,12 +1,68 @@
-import Team from "./cards.jsx/team-cards";
+import React, { useState } from "react";
 import './teams1.css'
-import React from 'react'
+import images from "../../../../assets/images/Card1-img.jpg";
 
 const Teams1 = () => {
+  const teamMembers = [
+    {
+      id: 1,
+      name: "John",
+      age: "29",
+      position: "Developer",
+      email: "john@email.com",
+      image: images,
+    },
+    {
+      id: 2,
+      name: "Jane",
+      age: "32",
+      position: "Designer",
+      email: "jane@email.com",
+      image: images,
+    },
+    {
+      id: 3,
+      name: "Alex",
+      age: "35",
+      position: "Marketing",
+      email: "alex@email.com",
+      image: images,
+    },
+    {
+      id: 4,
+      name: "Alex",
+      age: "35",
+      position: "Marketing",
+      email: "alex@email.com",
+      image: images,
+    },
+  ];
+
+  const [selectedMember, setSelectedMember] = useState(null);
+
+  const handleCardClick = (member) => {
+    setSelectedMember(member.id === selectedMember?.id ? null : member);
+  };
   return (
-    <div className="team-container" >
-        <h1 className="head">Our Team Members</h1>
-        <Team/>
+    <div className="team1" >
+      <h1 className="team1-head">Our Team Members</h1>
+      <div className="team1-Card">
+       <div className="team1-Card-main">
+        {teamMembers.map((member) => (
+          <div className="team1-Card-main-show" key={member.id} onClick={() => handleCardClick(member)}>
+            <img  src={member.image} alt={member.name} />
+            {selectedMember && selectedMember.id === member.id && (
+              <div className="team1-Card-main-info slideUp">
+                <h1>{selectedMember.name}</h1>
+                <h4>{selectedMember.age}</h4>
+                <h4>{selectedMember.position}</h4>
+                <h4>{selectedMember.email}</h4>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
     </div>
   )
 }
