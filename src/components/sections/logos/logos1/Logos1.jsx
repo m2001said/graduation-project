38026a2e3567f75f09bc2e5c5f070fd226157c56
SolyGh <125503517+SolyGh@users.noreply.github.com>
2023-12-08 08:15:50 -0800

    done

--- src/components/sections/logos/logos1/Logos1.jsx
+++ src/components/sections/logos/logos1/Logos1.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "./logos.css";
 
 const Logos1 = () => {
   const { logos } = useSelector((state) => state.template);
@@ -14,25 +15,3 @@ const Logos1 = () => {
 };
 
 export default Logos1;
-
-// import React from "react";
-// import "./logos.css";
-// import PhotoImage01 from "./01.svg";
-// import PhotoImage02 from "./02.svg";
-// import PhotoImage03 from "./03.svg";
-// import PhotoImage04 from "./04.svg";
-// import PhotoImage05 from "./05.svg";
-// const Logos1 = () => {
-//   return (
-
-//     <div className="logos-1">
-//       <img src={PhotoImage01} alt="" />
-//       <img src={PhotoImage02} alt="" />
-//       <img src={PhotoImage03} alt="" />
-//       <img src={PhotoImage04} alt="" />
-//       <img src={PhotoImage05} alt="" />
-//     </div>
-//   );
-// };
-
-// export default Logos1;
