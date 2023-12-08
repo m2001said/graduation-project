6dc99b51b7642f61615d24aae1a3a078ff3940f7
SolyGh <125503517+SolyGh@users.noreply.github.com>
2023-12-08 06:57:05 -0800

    logos-feature1-testimonials//dynamic

---
+++ src/components/sections/testimonials/testimonials1/Testimonials1.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PhotoImage from "./person1.jpg";
+import "./testimonials1.css";
+import { useSelector } from "react-redux";
+
+const Testimonials1 = () => {
+  const { testimonial } = useSelector((state) => state.template);
+
+  return (
+    <div className="testimonials">
+      <p className="Testimonials__title">{testimonial.title}</p>
+
+      <div className="testimonials-cards">
+        {testimonial.cards.map((card, index) => (
+          <div className="testimonials-card" key={index}>
+            <div className="testimonials-card-header">
+              <img className="testimonials-card-header-img" src={card.imgUrl} alt=" " />
+              <div className="testimonials-card-info">
+                <p>
+                  <b>{card.name}</b>
+                </p>
+                <p>{card.location}</p>
+              </div>
+            </div>
+            <p className="testimonials-card-body">{card.opinion}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials1;
--- src/pages/TrialDesign.jsx
+++ src/pages/TrialDesign.jsx
@@ -6,6 +6,9 @@ import App from "../../src/App";
 import Project1 from "../components/sections/projects/projects1/Project1";
 import Articles from "../components/sections/Items/items1/Items1";
 import Statictics1 from "../components/sections/statistics/statistics1/Statictics1";
+import Testimonials1 from "../components/sections/testimonials/testimonials1/Testimonials1";
+import Featrue1 from "../components/sections/features/feature1/Featrue1";
+import Logos1 from "../components/sections/logos/logos1/Logos1";
 
 import "../assets/css/globals1.css";
 const TrialDesign = () => {
@@ -17,6 +20,9 @@ const TrialDesign = () => {
       <Project1 />
       <Statictics1 />
       <Articles />
+      <Featrue1 />
+      <Testimonials1 />
+      <Logos1 />
     </div>
   );
 };
