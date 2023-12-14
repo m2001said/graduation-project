be09ef1902af3a35e58db95ce0763205551b182c
Abdo-Hosni123 <abdohosni12384@gmail.com>
2023-12-14 21:05:36 +0200

    adding assets of design 2

--- src/pages/TrialDesign2.jsx
+++ src/pages/TrialDesign2.jsx
@@ -1,8 +1,10 @@
 import "../assets/css/globals2.css";
+import Navbar2 from "../components/sections/navbars/navbar2/Navbar2";
 
 const TrialDesign2 = () => {
   return (
     <div>
+      <Navbar2 />
       <h1>TrialDesign2</h1>
     </div>
   );
