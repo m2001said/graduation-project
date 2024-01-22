89055ca0a64a03e441038650d5c08c5708bebd7c
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-01-22 15:30:18 +0200

    Update Footer6.jsx

--- src/components/sections/footers/footer6/Footer6.jsx
+++ src/components/sections/footers/footer6/Footer6.jsx
@@ -17,7 +17,7 @@ const Footer6 = () => {
                   <span>
                     <img src={footer.imgUrl} alt="" className="ico-Logo" />
                   </span>{" "}
-                    <img src={navbar.logo} alt="logo" className="design6-LogoIcon" />
+                    <img src={footer.logo} alt="logo" className="design6-LogoIcon" />
                 </h2>
                 <p>{footer.description}</p>
               </div>
