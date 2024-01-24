c6f99165b9ee2e3466436e71dc6bbd32e50ccf17
SolyGh <125503517+SolyGh@users.noreply.github.com>
2024-01-23 19:33:34 -0800

    check if its the proper  update

--- src/components/sections/navbars/navbar2/Navbar2.jsx
+++ src/components/sections/navbars/navbar2/Navbar2.jsx
@@ -23,34 +23,3 @@ const Navbar2 = () => {
 };
 
 export default Navbar2;
-
-// import React from "react";
-// import "./navbar2.css";
-
-// import { useState } from "react";
-
-// const Nav = () => {
-//   const [activeNav, setActiveNav] = useState("#");
-
-//   return (
-//     <nav>
-//       <a onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""} href="#">
-//         <FaHome />
-//       </a>
-//       <a href="#services2" onClick={() => setActiveNav("#services2")} className={activeNav === "#services2" ? "active" : "#services2"}>
-//         <FaInfoCircle />
-//       </a>
-//       <a href="#projects2" onClick={() => setActiveNav("#projects2")} className={activeNav === "#projects2" ? "active" : "#projects2"}>
-//         <FaBook />
-//       </a>
-//       <a href="#testimonials2" onClick={() => setActiveNav("#testimonials2")} className={activeNav === "#testimonials2" ? "active" : "testimonials2"}>
-//         <FaHandsHelping />
-//       </a>
-//       <a href="#contactUs2" onClick={() => setActiveNav("#contactUs2")} className={activeNav === "#contactUs2" ? "active" : "#contactUs2"}>
-//         <MdMessage />
-//       </a>
-//     </nav>
-//   );
-// };
-
-// export default Nav;
