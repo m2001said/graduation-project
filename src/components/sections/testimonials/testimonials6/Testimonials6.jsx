7114a32a5922ec9c92ea3a2a3fa5a938461af21d
abdelrahmanrgab <abdelrahmanragab787@gmail.com>
2024-01-25 20:42:35 +0200

    replace reactstrap with tailwind

--- src/components/sections/testimonials/testimonials6/Testimonials6.jsx
+++ src/components/sections/testimonials/testimonials6/Testimonials6.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
 import "./testimonials6.css";
 import { useSelector } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -11,31 +10,27 @@ const Testimonials6 = () => {
 
   return (
     <section className="design-6">
-      <Container>
-        <Row>
-          <Col lg="8" sm="12" md="12" className="m-auto">
-            <div className="slider__wrapper d-flex align-items-center gap-5  ">
-              <div className="slider__content w-50">
-                <h2 className="mb-4 ps-3">{testimonial.title}</h2>
-                <Swiper modules={[Navigation, Pagination, Autoplay]} spaceBetween={50} slidesPerView={1} autoplay={{ delay: 3000 }} className="mySwiper">
-                  {testimonial.cards.map((data, index) => (
-                    <SwiperSlide key={index}>
-                      <div className="single__testimonial">
-                        <p className="review__content">{data.content}</p>
-                        <h6>{data.name}</h6>
-                        <p>{data.role}</p>
-                      </div>
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
-              </div>
-              <div className="slider__img w-50">
-                <img src={testimonial.imgUrl} alt="testimonial_image" className="w-100" />
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <div className="container mx-auto">
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/2">
+            <h2 className="mb-4 ps-3">{testimonial.title}</h2>
+            <Swiper modules={[Navigation, Pagination, Autoplay]} spaceBetween={50} slidesPerView={1} autoplay={{ delay: 3000 }} className="mySwiper">
+              {testimonial.cards.map((data, index) => (
+                <SwiperSlide key={index}>
+                  <div className="single__testimonial">
+                    <p className="review__content">{data.content}</p>
+                    <h6>{data.name}</h6>
+                    <p>{data.role}</p>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="slider__img md:w-1/2">
+            <img src={testimonial.imgUrl} alt="testimonial_image" className="w-full" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
