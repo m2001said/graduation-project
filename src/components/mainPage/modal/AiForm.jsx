5ce720ce42fb7a264e0a69b79bfb8fbb1fb8e40f
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-01-30 18:55:03 +0000

    aiModal

---
+++ src/components/mainPage/modal/AiForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AiForm = () => (
+  <>
+    <h1>generate your website quickly with the artificial intelligence </h1>
+    <p>write an overview of your project idea in 3 lines</p>
+    <textarea name="" id="" cols="30" rows="10" placeholder="website overview"></textarea>
+    <button className="form-button">Build your website now</button>
+  </>
+);
+
+export default AiForm;
