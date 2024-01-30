72df84379d7cb254f9b31ef462a77c8d8e755112
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-01-30 19:30:36 +0000

    .

--- src/components/mainPage/modal/AiForm.jsx
+++ src/components/mainPage/modal/AiForm.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const AiForm = () => (
   <>
-    <h1>generate your website quickly with the artificial intelligence </h1>
-    <p>write an overview of your project idea in 3 lines</p>
+    <h1 className="ai-form-title">create your website quickly with the artificial intelligence </h1>
+    <p className="ai-form-description">write an overview of your project idea in 3 lines</p>
     <textarea name="" id="" cols="30" rows="10" placeholder="website overview"></textarea>
     <button className="form-button">Build your website now</button>
   </>
