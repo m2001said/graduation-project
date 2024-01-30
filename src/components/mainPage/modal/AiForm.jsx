9157dc53fab4c2b0bcabbbb58f53d26c5e5287a0
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-01-30 19:53:33 +0000

    ......

--- src/components/mainPage/modal/AiForm.jsx
+++ src/components/mainPage/modal/AiForm.jsx
@@ -4,8 +4,8 @@ const AiForm = () => (
   <>
     <h1 className="ai-form-title">create your website quickly with the artificial intelligence </h1>
     <p className="ai-form-description">write an overview of your project idea in 3 lines</p>
-    <textarea name="" id="" cols="30" rows="10" placeholder="website overview"></textarea>
-    <button className="form-button">Build your website now</button>
+    <textarea name="overview" id="websiteOverview" required autofocus cols="30" rows="10" placeholder="website overview"></textarea>
+    <button className="form-button form-2-button">Build your website now</button>
   </>
 );
 
--- src/components/mainPage/modal/DeployForm.jsx
+++ src/components/mainPage/modal/DeployForm.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const AiForm = () => (
   <>
     <h1> نم النشر </h1>
-    <span> لينك موقغك </span>
-    <P>Abdo.website.vercel.com</P>
-    <button className="form-button">Build your website now</button>
+    <span className="label"> لينك موقغك </span>
+    <P className="website-link">Abdo.website.vercel.com</P>
+    <button className="form-button form-2-button">Build your website now</button>
   </>
 );
 
