e638c640502fab8eeddb8e887a94ba5b938f5e11
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2024-02-02 22:17:19 +0200

    edit dashboard

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TrialDesign3 from "./pages/TrialDesign3";
 import TrialDesign4 from "./pages/TrialDesign4";
 import TrialDesign5 from "./pages/TrialDesign5";
 import TrialDesign6 from "./pages/TrialDesign6";
+import TrialDesign10 from "./pages/TrialDesign10";
 function App() {
   return (
     <>
@@ -19,6 +20,7 @@ function App() {
           <Route path="/trial-design4" element={<TrialDesign4 />} />
           <Route path="/trial-design5" element={<TrialDesign5 />} />
           <Route path="/trial-design6" element={<TrialDesign6 />} />
+          <Route path="/trial-design10" element={<TrialDesign10 />} />
         </Route>
         {/* <Route path="/trial-design1" element={<TrialDesign1 />} /> */}
 
