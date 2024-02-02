16d15808e2632519723f2bec20e8e754a67db1de
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-02-02 15:09:11 +0000

    add designcard

--- src/components/designsPage/sections/DesignsContainer.jsx
+++ src/components/designsPage/sections/DesignsContainer.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import "./designsContainer.css";
+import DesignCard from "./designsCard/DesignCard";
+import design2 from "../../../assets/images/designs/design2.png";
+import design3 from "../../../assets/images/designs/design3.png";
+
+const DesignsData = [
+  {
+    title: "DesignTitle",
+    description: " Design description Design description ",
+    imageUrl: design2,
+  },
+  {
+    title: "DesignTitle",
+    description: " Design description Design description ",
+    imageUrl: design3,
+  },
+];
+
 const DesignsContainer = () => {
   return (
     <>
       <div className="designs-section">
         <div className="container mx-auto px-4  py-4">
-          <h1 className="text-3xl font-bold tracking-tighter text-center text-white sm:text-4xl md:text-5xl lg:text-6xl/none">Our Designs</h1>
+          <h1 className="text-3xl font-bold tracking-tighter mb-4 text-center text-white sm:text-4xl md:text-5xl lg:text-6xl/none">Our Designs</h1>
+          <p className="text-gray-400 text-center">Explore our latest and greatest designs.</p>
+          <div className="designs-container mt-8 flex flex-wrap gap-4">
+            {DesignsData.map((design) => (
+              <DesignCard Designdata={design}></DesignCard>
+            ))}
+          </div>
         </div>
       </div>
     </>
--- src/components/designsPage/sections/designsCard/DesignCard.jsx
+++ src/components/designsPage/sections/designsCard/DesignCard.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "./designCard.css";
 
-const DesignCard = () => {
+const DesignCard = ({ Designdata }) => {
   return (
     <>
-      <h1>card</h1>
+      <div className="design-card rounded-lg overflow-hidden shadow-lg p-4">
+        <div className="design-Img-container">
+          <img src={Designdata.imageUrl} className="object-cover w-full" alt="design--Img" />
+        </div>
+        <div className="design-info py-2">
+          <h2 className="designs-title font-bold text-xl font-semibold">{Designdata.title}</h2>
+          <p className="designs-description text-gray-600 text-sm ">{Designdata.description}</p>
+        </div>
+        <div className="buttons-container pt-2 flex justify-between gap-2">
+          <button className=" text-white rounded-md px-4 py-2 Build-button">Build</button>
+          {/* <button className="border rounded-md px-4 py-2 Preview-button">Preview</button> */}
+        </div>
+      </div>
     </>
   );
 };
