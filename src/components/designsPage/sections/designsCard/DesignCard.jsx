f0f5e9bc16a21ce1fb45da240eb95af52c53bab0
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-02-02 15:38:14 +0000

    add Preview-button

--- src/components/designsPage/sections/designsCard/DesignCard.jsx
+++ src/components/designsPage/sections/designsCard/DesignCard.jsx
@@ -7,12 +7,16 @@ const DesignCard = ({ Designdata }) => {
       <div className="design-card rounded-lg overflow-hidden shadow-lg p-4">
         <div className="design-Img-container">
           <img src={Designdata.imageUrl} className="object-cover w-full" alt="design--Img" />
+          <button className="Preview-button flex justify-center items-center">
+            <img src="https://res.cloudinary.com/duc04fwdb/image/upload/v1706887574/jammal_photos/inkcorzh0n6jvtfvolgq.svg" alt="" />
+          </button>
         </div>
         <div className="design-info py-2">
           <h2 className="designs-title font-bold text-xl font-semibold">{Designdata.title}</h2>
           <p className="designs-description text-gray-600 text-sm ">{Designdata.description}</p>
         </div>
-        <div className="buttons-container pt-2 flex justify-between gap-2">
+        {/* flex justify-between gap-2 */}
+        <div className="buttons-container pt-2 ">
           <button className=" text-white rounded-md px-4 py-2 Build-button">Build</button>
           {/* <button className="border rounded-md px-4 py-2 Preview-button">Preview</button> */}
         </div>
