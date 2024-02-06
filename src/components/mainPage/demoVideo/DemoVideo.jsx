5bc4952e74ddf6aefedb7f9de129d6b3338fd1b4
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-02-06 20:28:52 +0000

    more animations

--- src/components/mainPage/demoVideo/DemoVideo.jsx
+++ src/components/mainPage/demoVideo/DemoVideo.jsx
@@ -17,12 +17,12 @@ const DemoVideo = () => {
       <div className="demo-section bg-gradient-to-r from-[#481b91] to-[#240d55]">
         <div className="container mx-auto text-center px-4 py-4">
           <div className="text-info">
-            <h1 data-aos="fade-up" data-aos-duration="1000" className="text-3xl font-bold tracking-tighter text-white sm:text-4xl md:text-5xl lg:text-6xl/none">How to Use Our Website</h1>
-            <p data-aos="fade-up" data-aos-duration="1000" className="mx-auto max-w-[700px] text-gray-200 md:text-xl ">
+            <h1 data-aos="fade-down" data-aos-duration="1000" className="text-3xl font-bold tracking-tighter text-white sm:text-4xl md:text-5xl lg:text-6xl/none">How to Use Our Website</h1>
+            <p data-aos="fade-down" data-aos-duration="1000" className="mx-auto max-w-[700px] text-gray-200 md:text-xl ">
               Watch the video below to understand how to navigate and use our website effectively.
             </p>
           </div>
-          <div className="demo-video py-2" data-aos="fade-up" data-aos-duration="1000" >
+          <div className="demo-video py-2" data-aos="fade-down" data-aos-duration="1000" >
             <video src={demo} controls></video>
           </div>
         </div>
