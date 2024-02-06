3e90326b56efcb07eea2940d2a98722e16e71470
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-02-06 19:16:52 +0000

    .

--- src/components/mainPage/modal/validationUtils.js
+++ src/components/mainPage/modal/validationUtils.js
@@ -1,13 +1,12 @@
 export const validate = (email, password) => {
-    if (!email || !password) {
-      return "Both fields are required.";
-    }
-    if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
-      return "Please enter a valid email address.";
-    }
-    if (password.length < 6) {
-      return "Password must be at least 6 characters long.";
-    }
-    return ""; // No error if validation is successful
-  };
-  
+  if (!email || !password) {
+    return "Both fields are required.";
+  }
+  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+    return "Please enter a valid email address.";
+  }
+  if (password.length < 6) {
+    return "Password must be at least 6 characters long.";
+  }
+  return ""; // No error if validation is successful
+};
