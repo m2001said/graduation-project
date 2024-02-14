5d5725a29ad496a3d5b82299913a419e8609369f
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-02-14 14:09:31 +0000

    update app routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/first */
 import React, { useState } from "react";
 import { Route, Routes } from "react-router";
 import MainNav from "./components/mainPage/mainNavbar/MainNav";
@@ -12,16 +11,7 @@ import BaseModal from "./components/mainPage/modal/BaseModal/BaseModal.jsx";
 import SigninLogin from "./components/mainPage/modal/SigninLogin.jsx";
 
 const trialDesignComponents = Array.from({ length: 18 }, (_, i) => require(`./pages/TrialDesign${i + 1}`).default);
-// eslint-disable-next-line import/first
-import TrialDesign1 from "./pages/TrialDesign1";
-import TrialDesign2 from "./pages/TrialDesign2";
-import TrialDesign3 from "./pages/TrialDesign3";
-import TrialDesign4 from "./pages/TrialDesign4";
-import TrialDesign5 from "./pages/TrialDesign5";
-import TrialDesign6 from "./pages/TrialDesign6";
-import TrialDesign7 from "./pages/TrialDesign7"; // Adjusted line
-import TrialDesign10 from "./pages/TrialDesign10";
-import TrialDesign14 from "./pages/TrialDesign14";
+
 
 
 function App() {
@@ -48,21 +38,22 @@ function App() {
           <SigninLogin setIsSignedIn={signIn} closeModal={closeModal} />
         </BaseModal>
       ) : null}
-
-      <Routes>
-        <Route path="/" element={<MainPage toggleModal={toggleModal} />} />
-        <Route element={<Dashboard />}>
-          <Route path="/trial-design1" element={<TrialDesign1 />} />
-          <Route path="/trial-design2" element={<TrialDesign2 />} />
-          <Route path="/trial-design3" element={<TrialDesign3 />} />
-          <Route path="/trial-design4" element={<TrialDesign4 />} />
-          <Route path="/trial-design5" element={<TrialDesign5 />} />
-          <Route path="/trial-design6" element={<TrialDesign6 />} />
-          <Route path="/trial-design7" element={<TrialDesign7 />} /> {/* Adjusted line */}
-          <Route path="/trial-design10" element={<TrialDesign10 />} />
-          <Route path="/trial-design14" element={<TrialDesign14 />} />
-        </Route>
-      </Routes>
+      <div style={{marginTop: "78px"}}>
+        <Routes>
+          <Route path="/" element={<MainPage toggleModal={toggleModal} />} />
+          <Route path="/designs" element={<DesignsPage />} />
+          <Route path="/page-craft" element={<BuildYourPage />} />
+          {trialDesignComponents.map((Component, index) => (
+            <Route path={`/preview-trial-design${index + 1}`} element={<Component />} />
+          ))}
+          <Route element={<Dashboard />}>
+            {trialDesignComponents.map((Component, index) => (
+              <Route path={`/build-trial-design${index + 1}`} element={<Component />} />
+            ))}
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 }
