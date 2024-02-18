540b86978f18de3d6c2a152b5e4b4fb78c30b69b
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-02-18 20:55:10 +0000

    Import components directly without using React.lazy

--- src/pages/OwnPage.jsx
+++ src/pages/OwnPage.jsx
@@ -1,26 +1,26 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const OwnPage = () => {
   const location = useLocation();
   const selectedData = location.state || {};
 
-  // Check if heroIndexSelected and testimonialIndexSelected are defined, otherwise set them to default values
+  // if Selected are defined, otherwise set them to default values
   const heroIndexSelected = selectedData.heroIndexSelected || 1;
   const testimonialIndexSelected = selectedData.testimonialIndexSelected || 1;
   const footerIndexSelected = selectedData.footerIndexSelected || 1;
-  
-  // Use import() for dynamic import
-  const Hero = lazy(() => import(`../components/sections/heros/hero${heroIndexSelected}/Hero${heroIndexSelected}`).then(module => ({ default: module.default })));
-  const Testimonial = lazy(() => import(`../components/sections/testimonials/testimonials${testimonialIndexSelected}/Testimonials${testimonialIndexSelected}`).then(module => ({ default: module.default })));
-  const Footer = lazy(() => import(`../components/sections/footers/footer${footerIndexSelected}/Footer${footerIndexSelected}`).then(module => ({ default: module.default })));
+
+  // Import components 
+  const Hero = require(`../components/sections/heros/hero${heroIndexSelected}/Hero${heroIndexSelected}`).default;
+  const Testimonial = require(`../components/sections/testimonials/testimonials${testimonialIndexSelected}/Testimonials${testimonialIndexSelected}`).default;
+  const Footer = require(`../components/sections/footers/footer${footerIndexSelected}/Footer${footerIndexSelected}`).default;
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <div>
       <Hero />
       <Testimonial />
       <Footer />
-    </Suspense>
+    </div>
   );
 };
 
