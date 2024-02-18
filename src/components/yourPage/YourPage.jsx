7082d5df26ca370f4100c7148f06a14ef292e42a
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-02-18 21:40:32 +0000

    add sectionsState

--- src/components/yourPage/YourPage.jsx
+++ src/components/yourPage/YourPage.jsx
@@ -6,81 +6,70 @@ import { heroData, testimonialData, footerData } from "./sectionsData";
 
 
 const YourPage = () => {
-  const [heroIndexSelected, setHeroIndexSelected] = useState(1);
-  const [testimonialIndexSelected, setTestimonialIndexSelected] = useState(1);
-  const [footerIndexSelected, setFooterIndexSelected] = useState(1);
+
+  const sectionNames = ['hero', 'testimonial', 'footer', 'nav', 'feature', 'cta', 'pricing', 'project', 'service', 'team', 'statistic'];
+  const [selectedIndices, setSelectedIndices] = useState(sectionNames.reduce((acc, curr) => ({ ...acc, [curr]: 1 }), {}));
 
   const navigate = useNavigate();
 
-  const handleHeroIndexChange = (e) => {
+  const handleIndexChange = (section) => (e) => {
     const { value } = e.target;
-    setHeroIndexSelected(parseInt(value, 10));
-  };
-
-  const handleTestimonialIndexChange = (e) => {
-    const { value } = e.target;
-    setTestimonialIndexSelected(parseInt(value, 10));
-  };
-
-  const handleFooterIndexChange = (e) => {
-    const { value } = e.target;
-    setFooterIndexSelected(parseInt(value, 10));
+    setSelectedIndices(prevState => ({ ...prevState, [section]: parseInt(value, 10) }));
   };
 
   const handleSubmit = () => {
-    navigate('/own-page', { state: { heroIndexSelected, testimonialIndexSelected,footerIndexSelected } });
+    navigate('/own-page', { state: { heroIndexSelected: selectedIndices.hero, testimonialIndexSelected: selectedIndices.testimonial, footerIndexSelected: selectedIndices.footer } });
   };
-
 
   return (
     <div className="own-page ">
-      <div className="container mx-auto px-4 py-4">
-        <h1 className="text-3xl font-bold tracking-tighter py-8 text-center text-white sm:text-4xl md:text-5xl lg:text-6xl/none">Build Your Own Page</h1>
-        <div className="group text">
-          <p className="title">Website description</p>
-          <textarea name="text" id="text"></textarea>
-        </div>
-
-        <div className="group">
-          <p className="title">Hero sections</p>
-          {heroData.map((hero) => (
-            <InputsGroup
-              key={hero.id}
-              data={hero}
-              selectedIndex={heroIndexSelected}
-              handleIndexChange={handleHeroIndexChange}
-            />
-          ))}
-        </div>
-
-        <div className="group">
-          <p className="title">Testimonial sections</p>
-          {testimonialData.map((testimonial) => (
-            <InputsGroup
-              key={testimonial.id}
-              data={testimonial}
-              selectedIndex={testimonialIndexSelected}
-              handleIndexChange={handleTestimonialIndexChange}
-            />
-          ))}
-        </div>
-
-        <div className="group">
-          <p className="title">footer sections</p>
-          {footerData.map((footer) => (
-            <InputsGroup
-              key={footer.id}
-              data={footer}
-              selectedIndex={footerIndexSelected}
-              handleIndexChange={handleFooterIndexChange}
-            />
-          ))}
-        </div>
-
-        <button className='generate-own-btn' onClick={handleSubmit}>Generate your website</button>
+    <div className="container mx-auto px-4 py-4">
+      <h1 className="text-3xl font-bold tracking-tighter py-8 text-center text-white sm:text-4xl md:text-5xl lg:text-6xl/none">Build Your Own Page</h1>
+      <div className="group text">
+        <p className="title">Website description</p>
+        <textarea name="text" id="text"></textarea>
       </div>
-
+  
+      <div className="group">
+        <p className="title">Hero sections</p>
+        {heroData.map((hero) => (
+          <InputsGroup
+            key={hero.id}
+            data={hero}
+            selectedIndex={selectedIndices.hero}
+            handleIndexChange={handleIndexChange('hero')}
+          />
+        ))}
+      </div>
+  
+      <div className="group">
+        <p className="title">Testimonial sections</p>
+        {testimonialData.map((testimonial) => (
+          <InputsGroup
+            key={testimonial.id}
+            data={testimonial}
+            selectedIndex={selectedIndices.testimonial}
+            handleIndexChange={handleIndexChange('testimonial')}
+          />
+        ))}
+      </div>
+  
+      <div className="group">
+        <p className="title">footer sections</p>
+        {footerData.map((footer) => (
+          <InputsGroup
+            key={footer.id}
+            data={footer}
+            selectedIndex={selectedIndices.footer}
+            handleIndexChange={handleIndexChange('footer')}
+          />
+        ))}
+      </div>
+  
+      <button className='generate-own-btn' onClick={handleSubmit}>Generate your website</button>
     </div>
+  </div>
+  
   );
 };
 
