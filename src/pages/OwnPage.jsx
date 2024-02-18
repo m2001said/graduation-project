357e7d8c1fa5b166940d1dafedddb358aff80e32
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-02-18 19:44:19 +0000

    style

--- src/pages/OwnPage.jsx
+++ src/pages/OwnPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const OwnPage = () => {
@@ -6,21 +6,21 @@ const OwnPage = () => {
   const selectedData = location.state || {};
 
   // Check if heroIndexSelected and testimonialIndexSelected are defined, otherwise set them to default values
-  const heroIndexSelected = selectedData.heroIndexSelected !== undefined ? selectedData.heroIndexSelected : 1;
-  const testimonialIndexSelected = selectedData.testimonialIndexSelected !== undefined ? selectedData.testimonialIndexSelected : 1;
-  const footerIndexSelected = selectedData.footerIndexSelected !== undefined ? selectedData.footerIndexSelected : 1;
-
+  const heroIndexSelected = selectedData.heroIndexSelected || 1;
+  const testimonialIndexSelected = selectedData.testimonialIndexSelected || 1;
+  const footerIndexSelected = selectedData.footerIndexSelected || 1;
+  
   // Use import() for dynamic import
-  const Hero = React.lazy(() => import(`../components/sections/heros/hero${heroIndexSelected}/Hero${heroIndexSelected}`).then(module => ({ default: module.default })));
-  const Testimonial = React.lazy(() => import(`../components/sections/testimonials/testimonials${testimonialIndexSelected}/Testimonials${testimonialIndexSelected}`).then(module => ({ default: module.default })));
-  const Footer = React.lazy(() => import(`../components/sections/footers/footer${footerIndexSelected}/Footer${footerIndexSelected}`).then(module => ({ default: module.default })));
+  const Hero = lazy(() => import(`../components/sections/heros/hero${heroIndexSelected}/Hero${heroIndexSelected}`).then(module => ({ default: module.default })));
+  const Testimonial = lazy(() => import(`../components/sections/testimonials/testimonials${testimonialIndexSelected}/Testimonials${testimonialIndexSelected}`).then(module => ({ default: module.default })));
+  const Footer = lazy(() => import(`../components/sections/footers/footer${footerIndexSelected}/Footer${footerIndexSelected}`).then(module => ({ default: module.default })));
 
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-        <Hero />
-        <Testimonial />
-        <Footer />
-    </React.Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Hero />
+      <Testimonial />
+      <Footer />
+    </Suspense>
   );
 };
 
