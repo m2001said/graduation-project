45b563d5be7d0111dd3738bdfc2f43816134e608
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-02-18 13:41:03 +0000

    add all available hero and Testimonial sections

--- src/components/yourPage/sectionsData.js
+++ src/components/yourPage/sectionsData.js
@@ -5,6 +5,12 @@ export const heroData = [
     { id: 2, name: 'Hero2', image: hero6, type: 'Hero' },
     // ... other hero sections  
     { id: 3, name: 'Hero3', image: hero6, type: 'Hero' },
+    { id: 4, name: 'Hero4', image: hero6, type: 'Hero' },
+    { id: 5, name: 'Hero5', image: hero6, type: 'Hero' },
+    { id: 6, name: 'Hero6', image: hero6, type: 'Hero' },
+    { id: 7, name: 'Hero7', image: hero6, type: 'Hero' },
+    { id: 10, name: 'Hero10', image: hero6, type: 'Hero' },
+    { id: 11, name: 'Hero11', image: hero6, type: 'Hero' },
 ];
 
 export const testimonialData = [
@@ -12,4 +18,7 @@ export const testimonialData = [
     { id: 2, name: 'Testimonial2', image: hero6, type: 'Testimonial' },
     // ... other testimonial sections 
     { id: 3, name: 'Testimonial3', image: hero6, type: 'Testimonial' },
+    { id: 6, name: 'Testimonial6', image: hero6, type: 'Testimonial' },
+    { id: 7, name: 'Testimonial7', image: hero6, type: 'Testimonial' },
+    { id: 11, name: 'Testimonial11', image: hero6, type: 'Testimonial' },
 ];
