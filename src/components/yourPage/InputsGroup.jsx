e1edac5fee842e72241f4fad6b85e9f26926f79f
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-02-20 16:26:10 +0000

    remove default value

--- src/components/yourPage/InputsGroup.jsx
+++ src/components/yourPage/InputsGroup.jsx
@@ -1,24 +1,26 @@
+// InputsGroup.jsx
 import React from "react";
 
 const InputsGroup = ({ data, selectedIndex, handleIndexChange }) => {
   return (
     <div>
-    <input
-      type="radio"
-      id={data.name}
-      name={data.type}
-      value={data.id}
-      checked={selectedIndex === data.id}
-      onChange={(e) => handleIndexChange(e)}
-    />
-    <label htmlFor={data.name}>
-      <div className="check-icon">
-        <img src="https://res.cloudinary.com/duc04fwdb/image/upload/v1708037268/jammal_photos/nme9bdbxqehtul4yzmd7.svg" 
-        alt="check-icon" />
-      </div>
-      <img src={data.image} alt={`${data.type} Section ${data.id}`} />
-    </label>
-  </div>
+      <input
+        type="radio"
+        id={data.name}
+        name={data.type}
+        value={data.id}
+        onChange={handleIndexChange}
+      />
+      <label htmlFor={data.name}>
+        <div className="check-icon">
+          <img
+            src="https://res.cloudinary.com/duc04fwdb/image/upload/v1708037268/jammal_photos/nme9bdbxqehtul4yzmd7.svg"
+            alt="check-icon"
+          />
+        </div>
+        <img src={data.image} alt={`${data.type} Section ${data.id}`} />
+      </label>
+    </div>
   );
 };
 
