5f99301bb5fc3930d8e13377d8efa9a3b6165f83
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-03-06 14:41:28 +0000

    style

--- src/components/sections/features/feature6/Feature6.jsx
+++ src/components/sections/features/feature6/Feature6.jsx
@@ -8,9 +8,9 @@ const Feature6 = () => {
   return (
     <section className="design-6" id="About">
       <div className="container mx-auto px-4">
-      <div className="flex flex-wrap -mx-4">
-        <div className="w-full lg:w-1/2 md:w-1/2 px-4">
-            <img src={features.imgUrl} alt="" className="w-full"/>
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full lg:w-1/2 md:w-1/2 px-4">
+            <img src={features.imgUrl} alt="" className="w-full" />
           </div>
 
           <div className="w-full lg:w-1/2 md:w-1/2 px-4">
@@ -20,40 +20,16 @@ const Feature6 = () => {
               <p>{features.info}</p>
             </div>
             <div className="features mt-4">
-              <div className="feature-row flex items-center gap-5">
-                <div className="single__feature">
-                  <span>
-                    <img src={features.cards[0].icon} alt="" className="ico" />
-                  </span>
-                  <h6>{features.cards[0].title}</h6>
-                  <p>{features.cards[0].description}</p>
-                </div>
-
-                <div className="single__feature">
-                  <span className="feature__icon-two">
-                    <img src={features.cards[1].icon} alt="" className="ico" />
-                  </span>
-                  <h6>{features.cards[1].title}</h6>
-                  <p>{features.cards[1].description}</p>
-                </div>
-              </div>
-
-              <div className="feature-row mt-3 flex items-center gap-5">
-                <div className="single__feature">
-                  <span className="feature__icon-3">
-                    <img src={features.cards[2].icon} alt="" className="ico" />
-                  </span>
-                  <h6>{features.cards[2].title}</h6>
-                  <p>{features.cards[2].description}</p>
-                </div>
-
-                <div className="single__feature">
-                  <span className="feature__icon-4">
-                    <img src={features.cards[3].icon} alt="" className="ico" />
-                  </span>
-                  <h6>{features.cards[3].title}</h6>
-                  <p>{features.cards[3].description}</p>
-                </div>
+              <div className="grid grid-cols-2 gap-5">
+                {features.cards.map((card, index) => (
+                  <div key={index} className="single__feature">
+                    <span>
+                      <img src={card.icon} alt="" className="ico" />
+                    </span>
+                    <h6>{card.title}</h6>
+                    <p>{card.description}</p>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
