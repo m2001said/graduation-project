3a6aa1ad06581943b4992ab29f61e674b16aaa04
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-03-18 22:11:30 +0000

    .

--- src/components/sections/features/feature72/Feature72.jsx
+++ src/components/sections/features/feature72/Feature72.jsx
@@ -1,29 +1,30 @@
-import styles, { layout } from "../../heros/hero7/styles7/style";
+import React from "react";
 import { useSelector } from "react-redux";
+import styles, { layout } from "../../heros/hero7/styles7/style";
 
 const Billing = () => {
-  const { feature } = useSelector((state) => state.template7);
+  const { characteristics } = useSelector((state) => state.template7);
   return (
     <section id="product" className={layout.sectionReverse}>
       <div className={layout.sectionImgReverse}>
-        <img src={feature.imgUrl[2]} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
-
+        <img src={characteristics.imgUrl[2]} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       </div>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
-          {feature.title.titleD} <br className="sm:block hidden" /> {feature.title.titleE}
+          {characteristics.title} <br className="sm:block hidden" /> {characteristics.subtitle}
         </h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`} style={{ color: "var(--color-dim-white)" }}>
-          {feature.title.titleF}{" "}
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`} style={{ color: "var(--color5)" }}>
+          {characteristics.content}
         </p>
         <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-          <img src={feature.imgUrl[0]} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
-          <img src={feature.imgUrl[1]} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+          <img src={characteristics.imgUrl[0]} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
+          <img src={characteristics.imgUrl[1]} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
         </div>
       </div>
     </section>
   );
 };
+
 export default Billing;
--- src/components/sections/features/feature73/Feature73.jsx
+++ src/components/sections/features/feature73/Feature73.jsx
@@ -1,24 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import styles, { layout } from "../../heros/hero7/styles7/style";
 import Button7 from "../feature7/Button7";
-import { useSelector } from "react-redux";
 
 const CardDeal = () => {
-  const { feature } = useSelector((state) => state.template7);
+  const { paymentMethods } = useSelector((state) => state.template7);
   return (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>
-          {feature.title.titleG} <br className="sm:block hidden" /> {feature.title.titleH}
+          {paymentMethods.title} <br className="sm:block hidden" /> {paymentMethods.subtitle}
         </h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`} style={{ color: "var(--color-dim-white)" }}>
-          {feature.title.titleK}{" "}
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`} style={{ color: "var(--color5)" }}>
+          {paymentMethods.content}
         </p>
-        <Button7 styles={`px-4 py-5 mt-6 bg-green-500 text-white rounded-full hover:bg-green-700 `} />
+        <Button7 text="Find a Deal" styles={`px-4 py-5 mt-6 text-[var(--color2)] rounded-full bg-[var(--color3)]`} />
       </div>
       <div className={layout.sectionImg}>
-        <img src={feature.imgUrl[3]} alt="billing" className="w-[100%] h-[100%]" />
+        <img src={paymentMethods.imgUrl} alt="billing" className="w-[100%] h-[100%]" />
       </div>
     </section>
   );
 };
+
 export default CardDeal;
