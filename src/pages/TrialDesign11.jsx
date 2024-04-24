bf21131f81d87ac5b42912c6fbd1f58c2c17fb27
ahmed <ahmed.mohamed24711#gmail.com>
2024-04-24 17:55:37 +0200

    all updates in 4/24/2024

--- src/pages/TrialDesign11.jsx
+++ src/pages/TrialDesign11.jsx
@@ -1,3 +1,4 @@
+
 import "../assets/css/globals11.css";
 import React from "react";
 import Navbar11 from "../components/sections/navbars/navbar11/Navbar11";
@@ -12,60 +13,22 @@ import Cta11 from "../components/sections/ctas/cta11/Cta11";
 import Contact11 from "../components/sections/contacts/contact11/Contact11";
 
 const TrialDesign11 = () => {
-  // const state = useSelector((state) => state.template1);
-  // const reorderedComponents = Object.keys(state);
-  // const componentMapping = {
-  //   hero: Hero11,
-  //   services: Services11,
-  //   services: Services11,
-  //   features: Featrue11,
-  //   testimonials: Testimonials11,
-  //   logos: Logos11,
-  //   projects: Project11,
-  //   statistics: Statistics11,
-  //   items: Items11,
-  //   team: Team11,
-  //   pricing: Pricing11,
-  //   cta: Cta11,
-  //   footer: Footer11,
-  // };
-  return (
-    <div>
-<<<<<<< HEAD
-      <Navbar11 />
-      <main className="main">
-        <Hero11 />
-        <Services11 />
-        <Services112 />
-        <Cta11 />
-        <Project11 />
-        <Pricings11 />
-        <Testimonials11 />
-        <Footer11 />
-        <Contact11 />
-        {/* {reorderedComponents.map((_component) => {
-          const Component = componentMapping[_component];
-          return Component && <Component key={_component} />;
-        })} */}
-      </main>
-=======
-      <>
-        <Navbar11 />
-        <main className="main">
-          <Hero11 />
-          <About11/>
-          <Services11 />
-          <Cta11 />
-          <Project11 />
-          <Pricings11 />
-          <Testimonials11 />
-          <Footer11 />
-          <Contact11 />
-        </main>
-      </>
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
-    </div>
-  );
+    return (
+        <div>
+            <Navbar11 />
+            <main className="main">
+                <Hero11 />
+                <About11 />
+                <Services11 />
+                <Cta11 />
+                <Project11 />
+                <Pricings11 />
+                <Testimonials11 />
+                <Footer11 />
+                <Contact11 />
+            </main>
+        </div>
+    );
 };
 
 export default TrialDesign11;
--- src/pages/TrialDesign3.jsx
+++ src/pages/TrialDesign3.jsx
@@ -1,62 +1,5 @@
-// import "../assets/css/globals3.css";
-<<<<<<< HEAD
-// import Navbar3 from "../components/sections/navbars/navbar3/Navbar3";
-=======
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
-// import Hero3 from "../components/sections/heros/hero3/Hero3";
-// import Project3 from "../components/sections/projects/project3/Project3";
-// import Feature3 from "../components/sections/features/feature3/Feature3";
-// import Pricing3 from "../components/sections/pricings/pricing3/Pricing3";
-// import Testimonials3 from "../components/sections/testimonials/testimonials3/Testimonials3";
-// import Cta3 from "../components/sections/ctas/cta3/Cta3";
-// import Footer3 from "../components/sections/footers/footer3/Footer3";
-// import { useSelector } from "react-redux";
-// const TrialDesign3 = () => {
-//   const state = useSelector((state) => state.template3);
-//   const reorderedComponents = Object.keys(state);
-//   const componentMapping = {
-<<<<<<< HEAD
-//     features: Feature3,
-//     testimonials: Testimonials3,
-//     projects: Project3,
-//     pricing: Pricing3,
-//     cta: Cta3,
-//   };
-//   return (
-//     <div className="TrialDesign3">
-//       <Hero3 />
-=======
-//     navbar: Navbar3,
-//     hero: Hero3,
-//     feature: Feature3,
-//     testimonial: Testimonials3,
-//     projects: Project3,
-//     pricing: Pricing3,
-//     cta: Cta3,
-//     footer: Footer3,
-//   };
-//   return (
-//     <div className="TrialDesign3">
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
-//       {reorderedComponents.map((_component) => {
-//         const Component = componentMapping[_component];
-//         return Component && <Component key={_component} />;
-//       })}
-<<<<<<< HEAD
-//       <Footer3 />
-=======
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
-//     </div>
-//   );
-// };
-
-// export default TrialDesign3;
 import React from "react";
-<<<<<<< HEAD
-import "../assets/css/globals3.css";
-=======
 import { useSelector } from "react-redux";
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
 import Navbar3 from "../components/sections/navbars/navbar3/Navbar3";
 import Hero3 from "../components/sections/heros/hero3/Hero3";
 import Project3 from "../components/sections/projects/project3/Project3";
@@ -65,49 +8,29 @@ import Pricing3 from "../components/sections/pricings/pricing3/Pricing3";
 import Testimonials3 from "../components/sections/testimonials/testimonials3/Testimonials3";
 import Cta3 from "../components/sections/ctas/cta3/Cta3";
 import Footer3 from "../components/sections/footers/footer3/Footer3";
-<<<<<<< HEAD
-import { useSelector } from "react-redux";
-=======
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
 
 const TrialDesign3 = () => {
-  const state = useSelector((state) => state.template3);
-  const reorderedComponents = Object.keys(state);
-  const componentMapping = {
-<<<<<<< HEAD
-    // feature: Feature3,
-    // testimonials: Testimonials3,
-    // project: Project3,
-    // pricing: Pricing3,
-    // cta: Cta3,
-=======
-    navbar: Navbar3,
-    hero: Hero3,
-    feature: Feature3,
-    testimonial: Testimonials3,
-    projects: Project3,
-    pricing: Pricing3,
-    cta: Cta3,
-    footer: Footer3,
->>>>>>> 57254c1011f83d273bba4f87a7b325d921012b30
-  };
+    const state = useSelector((state) => state.template3);
+    const reorderedComponents = Object.keys(state);
+    const componentMapping = {
+        hero: Hero3,
+        feature: Feature3,
+        testimonial: Testimonials3,
+        projects: Project3,
+        pricing: Pricing3,
+        cta: Cta3,
+        footer: Footer3,
+    };
 
-  return (
-    <div className="TrialDesign3">
-      <Navbar3 />
-      <Hero3 />
-      <Project3 />
-      <Feature3 />
-      <Pricing3 />
-      <Testimonials3 />
-      <Cta3 />
-      {reorderedComponents.map((component) => {
-        const Component = componentMapping[component];
-        return Component ? <Component key={component} /> : null;
-      })}
-      <Footer3 />
-    </div>
-  );
+    return (
+        <div className="TrialDesign3">
+            <Navbar3 />
+            {reorderedComponents.map((component) => {
+                const Component = componentMapping[component];
+                return Component ? <Component key={component} /> : null;
+            })}
+        </div>
+    );
 };
 
 export default TrialDesign3;
