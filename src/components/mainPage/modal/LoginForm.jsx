6b860e4d3994010917699036a5b71807c001bfa0
Abdelrahman Ragab <abdelrahmanragab787@gmail.com>
2024-05-01 21:04:25 +0000

    Add spin loader animation to LoginForm and SignUpForm

--- src/components/mainPage/modal/LoginForm.jsx
+++ src/components/mainPage/modal/LoginForm.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loginUserAsync } from '../../../features/auth/authSlice';
 import { useNavigate } from 'react-router-dom';
+import spinner from '../../../assets/images/spinner.svg';
 
-const LoginForm = ({ toggleForm ,toggleModal}) => {
-  const Navigate = useNavigate(); 
+const LoginForm = ({ toggleForm, toggleModal }) => {
+  const navigate = useNavigate(); // Changed from Navigate to navigate
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
+  const { status } = useSelector((state) => state.auth);
 
   const handleSignInClick = async () => {
-    dispatch(loginUserAsync({ email, password }));
-    toggleModal();
-    Navigate('/designs');
+    try {
+      await dispatch(loginUserAsync({ email, password }));
+      toggleModal();
+      navigate('/designs');
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
   };
-
 
   return (
     <>
@@ -24,9 +29,12 @@ const LoginForm = ({ toggleForm ,toggleModal}) => {
         <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <label htmlFor="password">Password</label>
         <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <div className="message-error">error</div>
-        <button className="form-button" onClick={handleSignInClick}>
+        <div className="message-error"></div>
+        <button className={`form-button ${status === 'loading' ? 'flex gap-8' : ''}`} onClick={handleSignInClick} disabled={status === 'loading'}>
           Login
+          {status === 'loading' && (
+            <img src={spinner} alt="Loading" className="loading animate-spin-slow w-20" />
+          )}
         </button>
         <div className="sub-button">
           <span> Don't have an account? </span>
--- src/components/mainPage/modal/SignUpForm.jsx
+++ src/components/mainPage/modal/SignUpForm.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { registerUserAsync } from '../../../features/auth/authSlice';
 import { validate } from './validationUtils';
+import spinner from '../../../assets/images/spinner.svg';
 
 const SignUpForm = ({ toggleForm }) => {
 
@@ -23,39 +24,30 @@ const SignUpForm = ({ toggleForm }) => {
 
 
   return (
-    <>
-      {status === 'succeeded' ? (
-        <div className="verification-message">
-          <h1>Verify Your Email</h1>
-          <p>Please check your email inbox to verify your email address before logging in.</p>
-          <div className="sub-button text-center">
-            <span> Already have an account? </span>
-            <button onClick={toggleForm}>Login</button>
-          </div>
-        </div>
-      ) : (
-        <>
-          <h1>Create Account</h1>
-          <div className="modal-form">
-            <label htmlFor="newName">Name</label>
-            <input type="text" id="newName" value={name} onChange={(e) => setName(e.target.value)} />
-            <label htmlFor="newEmail">Email</label>
-            <input type="email" id="newEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <label htmlFor="newPassword">Password</label>
-            <input type="password" id="newPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <div className="message-error">hhg</div>
-            {errorMessage && <p className="message-error">{errorMessage}</p>}
 
-            <button className="form-button" onClick={handleSignUp}>
-              Create Account
-            </button>
-            <div className="sub-button">
-              <span> Already have an account? </span>
-              <button onClick={toggleForm}>Login</button>
-            </div>
-          </div>
-        </>
-      )}
+    <>
+      <h1>Create Account</h1>
+      <div className="modal-form">
+        <label htmlFor="newName">Name</label>
+        <input type="text" id="newName" value={name} onChange={(e) => setName(e.target.value)} />
+        <label htmlFor="newEmail">Email</label>
+        <input type="email" id="newEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label htmlFor="newPassword">Password</label>
+        <input type="password" id="newPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div className="message-error"></div>
+        {errorMessage && <p className="message-error">{errorMessage}</p>}
+
+        <button className={`form-button ${status === 'loading' ? 'flex gap-8' : ''}`} onClick={handleSignUp} disabled={status === 'loading'}>
+          Create Account
+          {status === 'loading' && (
+            <img src={spinner} alt="Loading" className="loading animate-spin-slow " />
+          )}
+        </button>
+        <div className="sub-button">
+          <span> Already have an account? </span>
+          <button onClick={toggleForm}>Login</button>
+        </div>
+      </div>
     </>
   );
 };
