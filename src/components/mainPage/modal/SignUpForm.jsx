9d43f3c4a9677a0cd5a9058f8f87996aa69cc90a
Abdelrahman Ragab <abdelrahmanragab787@gmail.com>
2024-05-07 02:05:43 +0000

    verifiedmodel and update spinner.svg

--- src/components/mainPage/modal/SignUpForm.jsx
+++ src/components/mainPage/modal/SignUpForm.jsx
@@ -3,13 +3,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { registerUserAsync } from '../../../features/auth/authSlice';
 import { validate } from './validationUtils';
 import spinner from '../../../assets/images/spinner.svg';
+import Verified from './Verified';
+import { useEffect } from 'react';
 
 const SignUpForm = ({ toggleForm }) => {
-
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [createSuccess, setCreateSuccess] = useState(false);
+
   const dispatch = useDispatch();
   const { status } = useSelector((state) => state.auth);
 
@@ -19,37 +22,76 @@ const SignUpForm = ({ toggleForm }) => {
       setErrorMessage(validationError);
       return;
     }
-    dispatch(registerUserAsync({ name, email, password }));
+    await dispatch(registerUserAsync({ name, email, password }));
+    setCreateSuccess(true);
   };
 
+  // Clear form fields when createSuccess changes
+  useEffect(() => {
+    if (createSuccess) {
+      setName('');
+      setEmail('');
+      setPassword('');
+    }
+  }, [createSuccess]);
 
   return (
-
     <>
-      <h1>Create Account</h1>
-      <div className="modal-form">
-        <label htmlFor="newName">Name</label>
-        <input type="text" id="newName" value={name} onChange={(e) => setName(e.target.value)} />
-        <label htmlFor="newEmail">Email</label>
-        <input type="email" id="newEmail" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <label htmlFor="newPassword">Password</label>
-        <input type="password" id="newPassword" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <div className="message-error"></div>
-        {errorMessage && <p className="message-error">{errorMessage}</p>}
+      {createSuccess ? (
+      <Verified toggleForm={toggleForm} />
 
-        <button className={`form-button ${status === 'loading' ? 'flex gap-8' : ''}`} onClick={handleSignUp} disabled={status === 'loading'}>
-          Create Account
-          {status === 'loading' && (
-            <img src={spinner} alt="Loading" className="loading animate-spin-slow " />
-          )}
-        </button>
-        <div className="sub-button">
-          <span> Already have an account? </span>
-          <button onClick={toggleForm}>Login</button>
-        </div>
-      </div>
+      ) : (
+        <>
+          <h1>Create Account</h1>
+          <div className="modal-form">
+            <label htmlFor="newName">Name</label>
+            <input
+              type="text"
+              id="newName"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label htmlFor="newEmail">Email</label>
+            <input
+              type="email"
+              id="newEmail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="newPassword">Password</label>
+            <input
+              type="password"
+              id="newPassword"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="message-error"></div>
+            {errorMessage && <p className="message-error">{errorMessage}</p>}
+
+            <button
+              className={`form-button`}
+              onClick={handleSignUp}
+              disabled={status === 'loading'}
+            >
+              Create Account
+              {status === 'loading' && (
+                <img
+                  src={spinner}
+                  alt="Loading"
+                  className="loading animate-spin-slow "
+                />
+              )}
+            </button>
+            <div className="sub-button">
+              <span> Already have an account? </span>
+              <button onClick={toggleForm}>Login</button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
 
 export default SignUpForm;
+
