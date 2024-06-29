ca6b8fe2b6a527cb0bbb8288d4f56c5b58baf5d1
m2001said <ms277712@gmail.com>
2024-06-29 22:06:20 +0300

    adding translation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useNavigate, useLocation } from "react-router";
 import MainNav from "./components/mainPage/mainNavbar/MainNav";
 import Dashboard from "./pages/Dashboard";
 import MainPage from "./pages/MainPage";
@@ -8,34 +8,49 @@ import BuildYourPage from "./pages/BuildYourPage";
 import NotFound from "./pages/NotFoundPage";
 import OwnPage from "./pages/OwnPage.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute";
-
 import YourWebsites from "./pages/YourWebsites.jsx";
+import { useRightToLeft } from "./utils/rightToLeft.js";
+import { useTranslation } from "react-i18next";
+
 const trialDesignComponents = Array.from({ length: 18 }, (_, i) => require(`./pages/TrialDesign${i + 1}`).default);
 const websites = Array.from({ length: 18 }, (_, i) => require(`./pages/TrialDesign${i + 1}`).default);
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const isRightToLeft = useRightToLeft();
+  const { i18n } = useTranslation();
+  const language = i18n.language;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Redirect root URL to the current language
+    if (location.pathname === "/") {
+      navigate(`/${language}`);
+    }
+  }, [language, location.pathname, navigate]);
+
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
   return (
-    <>
+    <div className={isRightToLeft ? "rtl" : "ltr"}>
       <MainNav toggleModal={toggleModal} />
       <div style={{ marginTop: "77px" }} id="template-container">
         <Routes>
-          <Route path="/" element={<MainPage toggleModal={toggleModal} isModalOpen={isModalOpen} />} />
+          <Route path={`/${language}`} element={<MainPage toggleModal={toggleModal} isModalOpen={isModalOpen} />} />
           <Route
-            path="/designs"
+            path={`/${language}/designs`}
             element={
               <ProtectedRoute>
                 <DesignsPage />
               </ProtectedRoute>
             }
           />
-          <Route path="/websites" element={<YourWebsites />} />
+          <Route path={`/${language}/websites`} element={<YourWebsites />} />
           <Route
-            path="/page-craft"
+            path={`/${language}/page-craft`}
             element={
               <ProtectedRoute>
                 <BuildYourPage />
@@ -43,7 +58,7 @@ function App() {
             }
           />
           <Route
-            path="/own-page"
+            path={`/${language}/own-page`}
             element={
               <ProtectedRoute>
                 <OwnPage />
@@ -54,7 +69,7 @@ function App() {
           {trialDesignComponents.map((Component, index) => (
             <Route
               key={`preview-trial-design${index}`}
-              path={`/preview-trial-design${index + 1}`}
+              path={`/${language}/preview-trial-design${index + 1}`}
               element={
                 <ProtectedRoute>
                   <Component />
@@ -67,7 +82,7 @@ function App() {
           {websites.map((Component, index) => (
             <Route
               key={`zwep${index}`}
-              path={`/zweb${index + 1}`}
+              path={`/${language}/zweb${index + 1}`}
               element={
                 <ProtectedRoute>
                   <Component />
@@ -79,7 +94,7 @@ function App() {
             {websites.map((Component, index) => (
               <Route
                 key={`edit-zweb${index}`}
-                path={`/edit-zweb${index + 1}`}
+                path={`/${language}/edit-zweb${index + 1}`}
                 element={
                   <ProtectedRoute>
                     <Component />
@@ -89,14 +104,12 @@ function App() {
             ))}
           </Route>
 
-
-
           <Route element={<Dashboard />}>
             {trialDesignComponents.map((Component, index) => (
               // <Route key={`build-trial-design${index}`} path={`/build-trial-design${index + 1}/:pageId`} element={<Component />} />
               <Route
                 key={`build-trial-design${index}`}
-                path={`/build-trial-design${index + 1}`}
+                path={`/${language}/build-trial-design${index + 1}`}
                 element={
                   <ProtectedRoute>
                     <Component />
@@ -108,7 +121,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/mainPage/mainNavbar/MainNav.jsx
+++ src/components/mainPage/mainNavbar/MainNav.jsx
@@ -3,25 +3,38 @@ import { Link } from "react-router-dom";
 import "./mainNav.css";
 import logo from "../../../assets/images/mainPageAssets/logo.svg";
 import user from "../../../assets/images/mainPageAssets/user.svg";
+import translateArabic from "../../../assets/images/mainPageAssets/translateArabic.svg";
+import translateEnglish from "../../../assets/images/mainPageAssets/translateEnglish.svg";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUserAsync } from "../../../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { persistor } from "../../../app/store";
 
 const MainNav = ({ toggleModal }) => {
-  const Navigate = useNavigate();
+  const navigate = useNavigate();
   const authState = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const { i18n, t } = useTranslation("main");
 
   const handleSignOut = () => {
     dispatch(logoutUserAsync());
-    Navigate("/");
+    navigate("/");
+  };
+
+  const changeLanguage = (lng) => {
+    const currentPath = window.location.pathname;
+    const newPath = currentPath.replace(/^\/(en|ar)/, `/${lng}`);
+    i18n.changeLanguage(lng);
+    localStorage.setItem("language", lng);
+    navigate(newPath);
   };
 
   return (
     <>
       <div className="main-nav" id="main-nav">
         <div className="container mx-auto px-4 flex  justify-between items-center py-4">
-          <Link to={"/"}>
+          <Link to={`/${i18n.language}`}>
             <div className="main-logo flex items-center">
               <img src={logo} alt="logo" />
               <span>WEB</span>
@@ -37,8 +50,20 @@ const MainNav = ({ toggleModal }) => {
               </Link>
             )}
             <button className="signIn-btn p-1 md:p-2" onClick={authState.status === "succeeded" && authState.user ? handleSignOut : toggleModal}>
-              {authState.status === "succeeded" && authState.user ? "Sign Out" : "Sign In"}
+              {authState.status === "succeeded" && authState.user ? t("USER.LOG_OUT") : t("USER.SIGN_IN")}
             </button>
+            <div>
+              {i18n.language === "ar" && (
+                <button onClick={() => changeLanguage("en")}>
+                  <img className="w-8" src={translateArabic} alt="translateArabic" />
+                </button>
+              )}
+              {i18n.language === "en" && (
+                <button onClick={() => changeLanguage("ar")}>
+                  <img className="w-8" src={translateEnglish} alt="translateEnglish" />
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
