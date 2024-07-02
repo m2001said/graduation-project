c0cac28d2ef93be809853084adc13a2bb3967ed4
SolyGh <125503517+SolyGh@users.noreply.github.com>
2024-07-02 03:17:14 +0300

    2/7/2024 some design modifications and revised template slices and some styles

--- src/components/sections/navbars/navbar2/Navbar2.jsx
+++ src/components/sections/navbars/navbar2/Navbar2.jsx
@@ -8,8 +8,14 @@ const Navbar2 = ({ template }) => {
   return (
     <nav className="nav2">
       {navbar.links.map((item) => (
-        <a key={item.url} href={item.url} onClick={() => setActiveNav(item.url)} className={activeNav === item.url ? "active" : ""}>
-          <img src={item.imgUrl} alt="" />
+        <a
+          key={item.url}
+          href={item.url}
+          onClick={() => setActiveNav(item.url)}
+          className={activeNav === item.url ? "active" : ""}
+        >
+          <img src={item.imgUrl} alt="" className="nav-icon" />
+          <span className="nav-text">{item.text}</span>
         </a>
       ))}
     </nav>
@@ -17,37 +23,3 @@ const Navbar2 = ({ template }) => {
 };
 
 export default Navbar2;
-
-// import React, { useState } from "react";
-// import "./navbar2.css";
-// import { useSelector } from "react-redux";
-// import { useLocation } from "react-router";
-
-// const Navbar2 = () => {
-//   const [activeNav, setActiveNav] = useState("#");
-//   const { pathname } = useLocation();
-//   const navbar = useSelector((state) => {
-//     if (pathname.includes("own-page")) {
-//       return state.ownTemplate.navbar;
-//     } else {
-//       return state.template2.navbar;
-//     }
-//   });
-//   return (
-//     <nav className="nav2">
-//       {navbar.map((item) => (
-//         <a key={item.url} href={`#${item.url}`} onClick={() => setActiveNav(`#${item.url}`)} className={activeNav === `#${item.url}` ? "active" : ""}>
-//           <img src={item.imgUrl} alt="" />
-
-//           {item.url === "home"}
-//           {item.url === "services2"}
-//           {item.url === "projects2"}
-//           {item.url === "testimonials2"}
-//           {item.url === "contactUs2"}
-//         </a>
-//       ))}
-//     </nav>
-//   );
-// };
-
-// export default Navbar2;
