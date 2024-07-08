3231fb0777efcf2c54eaab3705165fbc00aadac2
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2024-07-08 22:12:48 +0300

    edit in team5 featurre5 & trial 1

--- src/components/sections/features/feature5/Feature5.jsx
+++ src/components/sections/features/feature5/Feature5.jsx
@@ -6,7 +6,7 @@ import "./feature5_1.css";
 const Feature5 = ({template}) => {
 const feature1 = template.features;
 
-return(
+return (
   <div className="feature5_1 design5-section design5">
     <div className="feature5_1-mainSection">
       <div className="feature5_1-title">
